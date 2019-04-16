895dcc048bbc49da18ef7690e236388ae9a4fe96
Sebastian Toivonen <sebastian.toivonen@gmail.com>
2019-04-16 17:35:42 +0300

    Update Entry

--- Game Engine/src/Entry.java
+++ Game Engine/src/Entry.java
@@ -4,7 +4,6 @@ import org.martin.scene.*;
 public class Entry {
 	public static void main(String[] args) {
 		CoreEngine engine = new CoreEngine(1280, 720, "Game Engine", 120);
-		engine.start(new Scene());
-		//
+		engine.start(new Scene2D());
 	}
 }
